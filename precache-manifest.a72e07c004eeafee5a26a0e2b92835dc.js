803dfae772d508b0aecf93481a519c0900e4e861
Amartya Mishra <kushmishra8@gmail.com>
2020-04-17 22:08:30 +0530

    Updates

---
+++ precache-manifest.a72e07c004eeafee5a26a0e2b92835dc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2b2c8868041c394b232a96c3c494570",
+    "url": "/movieShowcase/index.html"
+  },
+  {
+    "revision": "76dff3f83164b137c85e",
+    "url": "/movieShowcase/static/css/2.1e7836d9.chunk.css"
+  },
+  {
+    "revision": "3fff12279804d7b79212",
+    "url": "/movieShowcase/static/css/main.28f541f2.chunk.css"
+  },
+  {
+    "revision": "76dff3f83164b137c85e",
+    "url": "/movieShowcase/static/js/2.bbdb6493.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/movieShowcase/static/js/2.bbdb6493.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fff12279804d7b79212",
+    "url": "/movieShowcase/static/js/main.24f41543.chunk.js"
+  },
+  {
+    "revision": "11b2789d62ae5d76f168",
+    "url": "/movieShowcase/static/js/runtime-main.008bee5e.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/movieShowcase/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/movieShowcase/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/movieShowcase/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/movieShowcase/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/movieShowcase/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
